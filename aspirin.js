e787911ed3d1c0415762ac52e5d5a26a48a18afa
Selman Samet <selmansametun@gmail.com>
2019-05-11 23:19:16 +0300

    Prepared for submit to addons-mozilla

--- aspirin.js
+++ aspirin.js
@@ -5,26 +5,25 @@ function delete_tweet(tweets){
             let context = tweets[i].querySelector('.context');
             if(context.innerText){
                 let h = context.querySelector('.tweet-context>span')
-                console.log(h.className.split("--").splice(-1)[0])
-                tweets[i].remove();
+                let act = h.className.split("--").splice(-1)[0]
+                if(act !== "retweeted"){
+                    tweets[i].remove();
+                }
             }
         }
     }
 }
 
 const target = document.querySelector('.stream-items');
-
 var observer = new MutationObserver((mutations) => {
     mutations.forEach(mutation => {
         let tweets = [...mutation.addedNodes].map(node => node.childNodes[1])
         delete_tweet(tweets);
     })
 });
-
 var config = {
     attributes: true, childList: true, characterData: true
 };
-
 observer.observe(target, config) 
 
 delete_tweet([...document.querySelectorAll('.tweet')])
